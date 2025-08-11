5d57980d91982ef0c90221d09825332849832b06
sometimes305 <shigerunda@gmail.com>
2025-08-11 19:21:37 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// public フォルダ内のファイルを配信
+app.use(express.static(path.join(__dirname, "public")));
+
+// ここで既存のWS処理やAPIルートを定義
+
+
 import { WebSocketServer } from 'ws';
 import http from 'http';
 
